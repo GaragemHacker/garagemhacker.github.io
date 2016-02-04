c9ea661d57a5e9252d0b20aacf0c3f1add879549
Ellison Leão <ellisonleao@gmail.com>
2016-02-04 13:16:28 -0200

    initial layout changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Garagem'
 SITENAME = u'Garagem Hacker'
+SITESUBTITLE = u'Makerspace'
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
@@ -27,11 +28,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
+"""
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
-
+"""
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
