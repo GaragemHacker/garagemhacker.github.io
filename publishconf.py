5e8adb4e28d1002a2dc408ca2b828f415b838586
Ellison Leão <ellisonleao@gmail.com>
2017-01-11 16:50:04 -0200

    change siteurl

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://garagemhacker.com'
+SITEURL = 'http://garagemhacker.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
