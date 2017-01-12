65f3f6f8fa9242ffc61e4dad188dd4cc381cd158
Igor Brandao <igor.brandao@maisreaultado.com.br>
2017-01-12 11:04:54 -0200

    fixed publish settings

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://garagemhacker.com'
+SITEURL = 'http://garagemhacker.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
